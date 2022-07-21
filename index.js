4700338c6836069394c9729fa41a4b9d05957a4e
Theresa2013 <tab61295@gmail.com>
2022-07-20 20:28:22 -0500

    Add in classes

---
+++ index.js
@@ -0,0 +1,40 @@
+Employee:
+name
+id
+email
+getName()
+getId()
+getEmail()
+getRole() // Returns 'Employee'
+
+Manager:
+name
+id
+email
+officeNumber
+getName()
+getId()
+getEmail()
+getRole() // Overridden to return 'Manager'
+
+Engineer:
+name
+id
+email
+github //github username
+getName()
+getId()
+getEmail()
+getGithub()
+getRole() // Overridden to return 'Engineer'
+
+Intern:
+name
+id
+email
+school
+getName()
+getId()
+getEmail()
+getSchool()
+getRole() // Overridden to return 'Intern'
