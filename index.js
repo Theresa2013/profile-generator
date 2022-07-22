5a9937207e2b889f7e4cdc337311b9a5b2472689
Theresa2013 <tab61295@gmail.com>
2022-07-22 10:47:18 -0500

    tried reinstalling packages

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const generateHTML = require('./src/generate-site.js');
-const fs = require("fs");
-const path = require("path");
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const fs = require('fs');
+const path = require('path');
+const OUTPUT_DIR = path.resolve(__dirname, 'output')
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 const teamMembers = [];
 
 const promptManager = () => {
