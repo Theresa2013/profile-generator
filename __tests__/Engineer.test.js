21b273eb3f0e38ecacb2d63bc8ffb53329933132
Theresa2013 <tab61295@gmail.com>
2022-07-22 09:38:52 -0500

    Create Engineer object and pass all tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,31 @@
+
+
+const Engineer = require('../lib/Engineer');
+const engineer = new Engineer('theresa', '5', 'tab61295@gmail.com', 'Theresa2013');
+
+test('creates new engineer object', () => {
+    expect(engineer.name).toBe('theresa');
+    expect(engineer.id).toBe('5');
+    expect(engineer.email).toBe('tab61295@gmail.com');
+    expect(engineer.github).toBe('Theresa2013');
+});
+
+test('gets engineer name from the getName() method', () => {
+    expect(engineer.getName()).toBe('theresa');
+});
+
+test('gets engineer id from the getId() method', () => {
+    expect(engineer.getId()).toBe('5');
+});
+
+test('gets engineer email from the getEmail() method', () => {
+    expect(engineer.getEmail()).toBe('tab61295@gmail.com');
+});
+
+test('gets engineer github username from the getGithub() method', () => {
+    expect(engineer.getGithub()).toBe('Theresa2013');
+});
+
+test('gets engineer role from the getRole() method', () => {
+    expect(engineer.getRole()).toBe('Engineer');
+});
